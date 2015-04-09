00093a897dc0ed98add07f767dc2f5f2e5e1b155
Ronald Portier (Therp BV) <rportier@therp.nl>
2015-04-09 20:03:33 +0200

    [NEW] Initial commit.

---
+++ activity_flow/README.rst
@@ -0,0 +1,13 @@
+This module concerns itself with activities and workflow.
+
+The purpose is for organisations and/or individuals to be able to organise
+their work, coöperate, make sure all planned work gets done.
+
+Activities can be started and developed ad-hoc, or it is possible to define
+activity-types that consist of well defined steps.
+
+Activities can be assigned to groups or to individuals. There will be fine-
+grained control on who can see, or change activities.
+
+For further information have a look at the documentation.
+
---
+++ activity_flow/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    Odoo, Open Source Management Solution
+#    This module copyright (C) 2015 Portier Company BV
+#       http://portiercompany.com.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Organise activities',
+    'category': 'Getting organized',
+    'version': '1.0',
+    'author': 'Ronald Portier',
+    'website': 'http://therp.nl',
+    'depends': [
+        'document',
+    ],
+    'data': [
+    ],
+}
